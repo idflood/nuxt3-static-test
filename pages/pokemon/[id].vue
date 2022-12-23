5def62026ef4ff15513e49bbb4553c85e3d68f24
David Mignot <197418+idflood@users.noreply.github.com>
2022-12-23 07:06:38 +0100

    Use GqlInstance in details too

--- pages/pokemon/[id].vue
+++ pages/pokemon/[id].vue
@@ -9,11 +9,9 @@
 
 <script lang="ts" setup>
 const route = useRoute()
+const GqlInstance = useGql()
 
-const { data, error } = await useAsyncGql({
-  operation: 'pokemon_v2_pokemon_by_pk',
-  variables: { id: route.params.id },
-})
+const data = await GqlInstance('pokemon_v2_pokemon_by_pk', { id: route.params.id })
 const pokemon = data?.pokemon_v2_pokemon_by_pk
 
 </script>
